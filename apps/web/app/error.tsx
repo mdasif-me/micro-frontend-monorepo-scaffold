d73e2a49d74535e9312aee3e30b0d5f043287451
mdasif-me <cn.mdasif@gmail.com>
2026-05-11 20:59:00 +0600

    fix: internal server error page from the global feat: create new page not found page.

--- apps/web/app/error.tsx
+++ apps/web/app/error.tsx
@@ -2,8 +2,10 @@
 import Image from 'next/image';
 import icon from '../public/icons/chevron-right.svg';
 import img from '../public/images/global/internal-server-error.png';
+import network from '../public/images/global/network.png';
+import server from '../public/images/global/server.png';
 import { Icon } from './shared';
-import styles from './styles/error.module.css';
+import styles from './styles/global.module.css';
 
 export default function GlobalError({
   error,
@@ -18,6 +20,15 @@ export default function GlobalError({
       <body className={styles.background}>
         <div className={styles.container}>
           <Image
+            src={server}
+            alt='server'
+            width={345}
+            height={293}
+            draggable={false}
+            className={styles.top_img}
+            aria-label='server'
+          />
+          <Image
             src={img}
             alt='Internal Server Error'
             width={607}
@@ -25,11 +36,20 @@ export default function GlobalError({
             draggable={false}
             className={styles.image}
           />
+          <Image
+            src={network}
+            alt='Network'
+            width={345}
+            height={293}
+            draggable={false}
+            className={styles.bottom_img}
+            aria-label='network'
+          />
           <h2 className={styles.title}>Internal Server Error</h2>
-          <p
-            className={styles.message}
-          >{`it looks like the page you're looking for doesn't exist or might have been moved. don't worry, let's get you back on track!`}</p>
-          <button onClick={() => unstable_retry()}>
+          <p className={styles.message}>
+            {`it looks like something went wrong on our end. don't worry, let's get you back on track!`}
+          </p>
+          <button className={styles.button} onClick={() => unstable_retry()}>
             Try again <Icon src={icon} alt='chevron-right-icon' />
           </button>
         </div>
--- apps/web/app/not-found.tsx
+++ apps/web/app/not-found.tsx
@@ -1,17 +1,57 @@
 import type { Metadata } from 'next';
+import Image from 'next/image';
+import Link from 'next/link';
+import icon from '../public/icons/chevron-right.svg';
+import alien from '../public/images/global/alien.png';
+import network from '../public/images/global/network.png';
+import img from '../public/images/global/page-not-found.png';
+import { Icon } from './shared';
+import styles from './styles/global.module.css';
 
 export const metadata: Metadata = {
-  title: 'Not Found',
-  description: 'The page you are looking for does not exist.',
+  title: 'Page not found',
+  description: `it looks like the page you're looking for doesn't exist or might have been moved. don't worry, let's get you back on track!`,
 };
 
 export default function GlobalNotFound() {
   return (
     <html lang='en'>
-      <body>
-        <div>
-          <h1>Not Found</h1>
-          <p>The page you are looking for does not exist.</p>
+      <body className={styles.background}>
+        <div className={styles.container}>
+          <Image
+            src={alien}
+            alt='alien'
+            width={345}
+            height={293}
+            draggable={false}
+            className={styles.top_img}
+            aria-label='alien'
+          />
+          <Image
+            src={img}
+            alt='Page Not Found'
+            width={607}
+            height={372}
+            draggable={false}
+            className={styles.image}
+          />
+          <Image
+            src={network}
+            alt='Network'
+            width={345}
+            height={293}
+            draggable={false}
+            className={styles.bottom_img}
+            aria-label='network'
+          />
+          <h2 className={styles.title}>Page Not Found</h2>
+          <p
+            className={styles.message}
+          >{`it looks like the page you're looking for doesn't exist or might have been moved. don't worry, let's get you back on track!`}</p>
+          <Link href='/' className={styles.button}>
+            Go Home
+            <Icon src={icon} alt='chevron-right-icon' />
+          </Link>
         </div>
       </body>
     </html>
